53ebb84cffe71e3504ccfec44b218e70ca8fdf62
sejung2 <twindrop@naver.com>
2025-05-22 23:37:55 +0900

    feat: 문제관련 이벤트 추가

--- src/socket/events.js
+++ src/socket/events.js
@@ -5,8 +5,12 @@ module.exports = {
   JOIN_CLASSROOM: "joinClassroom",
   LEAVE_CLASSROOM: "leaveClassroom",
   SEND_MESSAGE: "sendMessage",
-  EDITOR_CONTENT_CHANGE: "editorContentChange",
+  EDITOR_CONTENT_CHANGE: "editorContentChange", // 폐기?
+  // 문제 관련
+  SELECT_PROBLEM_SET: "selectProblemSet", // 문제 선택
+  START_ACTIVITY: "startActivity", // 문제 풀이 시작
 
+  //서버 <-> 클라이언트
   REFRESH_PARTICIPANT_LIST: "refreshParticipantList",
 
   // 서버 -> 클라이언트
@@ -17,7 +21,10 @@ module.exports = {
   CLASSROOM_MESSAGE: "classroomMessage",
   MESSAGE_ERROR: "messageError",
   CLASSROOM_DELETED: "classroomDeleted",
-  EDITOR_STATE_SYNC: "editorStateSync",
+  EDITOR_STATE_SYNC: "editorStateSync", // 폐기?
+  // 문제 관련
+  PROBLEM_SELCTED_INFO: "problemSelectedInfo", // 선택된 문제 정보 전달
+  ACTIVITY_BEGIN: "activityBegin", // 문제 풀이 시작
 
   // 공통/내장
   CONNECT: "connect",
